e277fc167b1ee7693cedf8fab0f2ba82b9d07600
Maria-Malanova <121349457+Maria-Malanova@users.noreply.github.com>
2022-12-24 18:09:06 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const btnMore = document.querySelector('.articles-more');
+const articlesItem = document.querySelectorAll('.articles-item');
+
+btnMore.addEventListener('click',
+
+function() {
+
+    articlesItem.forEach(el => {
+        el.classList.add('articles-item--visible')
+    });
+    btnMore.closest('.articles-centre').classList.add('articles-centre--hiden');
+
+});
+
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.nav__link');
+
+burger.addEventListener('click',
+
+function() {
+    burger.classList.toggle('burger--active');
+
+    menu.classList.toggle('header__nav--active');
+
+    document.body.classList.toggle('stop-scroll');
+})
+
+menuLinks.forEach(function(el) {
+    el.addEventListener('click', function() {
+
+        burger.classList.remove('burger--active');
+
+        menu.classList.remove('header__nav--active');
+
+        document.body.classList.remove('stop-scroll');
+
+    })
+})
